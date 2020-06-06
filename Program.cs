2b70744320ae3753bcc520a85b225ac7a2d16130
Anton Lukyanov <a.lukyanov@itransition.com>
2020-06-06 18:30:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RuntasticExportProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
